1171210b65feae0e24c0f826da388d0ab297e177
brendatamaar <brendatama10@gmail.com>
2026-05-05 10:46:46 +0700

    feat(ui): configure react build env

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,18 +1,14 @@
 import { fileURLToPath, URL } from "node:url";
-import vue from "@vitejs/plugin-vue";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [
-    vue(),
+    react(),
     nodePolyfills({
       include: ["buffer", "path", "util"],
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true
-      }
+      globals: { Buffer: true, global: true, process: true }
     })
   ],
   resolve: {
