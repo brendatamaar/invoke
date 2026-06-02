2ec6bd4c1d560f8fb098b3be90992ad32409868c
brendatamaar <brendatama10@gmail.com>
2026-06-02 14:53:36 +0700

    fix: use PROTO_PATH env var for proto file resolution in Docker

--- packages/server/src/grpc/executor-client.ts
+++ packages/server/src/grpc/executor-client.ts
@@ -4,8 +4,7 @@ import grpc from "@grpc/grpc-js";
 import protoLoader from "@grpc/proto-loader";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const root = resolve(__dirname, "../../../..");
-const protoPath = resolve(root, "proto/executor.proto");
+const protoPath = process.env.PROTO_PATH ?? resolve(__dirname, "../../../../proto/executor.proto");
 
 const packageDefinition = protoLoader.loadSync(protoPath, {
   keepCase: false,
